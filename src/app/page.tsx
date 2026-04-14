aa80fc9d906ac30f4d929186adceba44bd4bd5f1
sgautier-dev <sgautier.dev@gmail.com>
2026-04-14 15:03:59 +0200

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/About"
 import Contact from "@/components/Contact"
 import Experience from "@/components/Experience"
 import FeaturedService from "@/components/FeaturedService"
+import Footer from "@/components/Footer"
 import Header from "@/components/Header"
 import Hero from "@/components/Hero"
 import Location from "@/components/Location"
@@ -20,6 +21,7 @@ export default function HomePage() {
 			<Universe />
 			<Location />
 			<Contact />
+			<Footer />
 		</main>
 	)
 }
